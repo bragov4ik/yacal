b35afbc82908143ba13ccfd6d48968486e00987f
Lev Lymarenko <lymarenko.lev@gmail.com>
2022-02-10 14:03:25 +0300

    = pp to fmt

--- cmd/yacal/main.go
+++ cmd/yacal/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/db47h/lex"
-	"github.com/k0kubun/pp"
 
 	"github.com/bragov4ik/yacal/pkg/lexer"
 	"github.com/bragov4ik/yacal/pkg/parser"
@@ -16,7 +16,7 @@ func main() {
 	for _, path := range os.Args[1:] {
 		file, err := os.Open(path)
 		if err != nil {
-			pp.Fatalf("Failed to open file %v: %v", path, err)
+			fmt.Printf("Failed to open file %v: %v", path, err)
 		}
 
 		f := lex.NewFile(path, file)
@@ -25,11 +25,11 @@ func main() {
 		ast, err := p.Parse()
 
 		if err != nil {
-			pp.Fatalf("Got an error while building an ast for file %v: %v", path, err)
+			fmt.Printf("Got an error while building an ast for file %v: %v", path, err)
 		}
 
 		global_ast = append(global_ast, ast...)
 	}
 
-	pp.Printf("%v\n", global_ast)
+	fmt.Printf("%v\n", global_ast)
 }
